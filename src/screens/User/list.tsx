ce91f80b72e696a0e3191330e13da183bdd0f0d2
Jeremiah Yoder <jeremiah.yoder@hotmail.com>
2023-02-22 12:28:13 -0700

    navigation integration. basic screen setup

---
+++ src/screens/User/list.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function List() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>User List</Text>
+    </View>
+  );
+}
+
+export default List;
